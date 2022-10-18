6fae69e6c4cf656393c831d4da263229fa3eb3f2
fei.deng <fei.deng@amlogic.com>
2022-10-18 18:50:56 +0800

    amlvideosink: CF2 add features to so lib [1/1]
    PD#SWPL-97684
    
    Problem:
    add features to so lib
    
    Solution:
    add features to so lib
    
    Verify:
    ap222
    
    Change-Id: I4a2e655beeab1016d5423f06541ea1f496428828
    Signed-off-by: fei.deng <fei.deng@amlogic.com>

--- src/aml_version.h
+++ src/aml_version.h
@@ -8,6 +8,14 @@ extern "C" {
 const char libVersion[]=
 "MM-module-name:gst-plugin-video-sink,version:1.0.1-r34-gfe3d6de";
 
+const char libFeatures[]=
+"MM-module-feature: set video full screen \n" \
+"MM-module-feature: Mute video frame \n" \
+"MM-module-feature: set av sync mode \n" \
+"MM-module-feature: set window size rectangle \n" \
+"MM-module-feature: set video as pip \n" \
+"MM-module-feature: support render lib for a/v sync and display\n";
+
 #ifdef  __cplusplus
 }
 #endif

575a5b68f067c69d6568a6be17a42a4c7ee1a516
guoping.li <guoping.li@amlogic.com>
2024-05-08 16:34:04 +0800

    videosink: Remove build result from source folder [1/2]
    PD#SWPL-151598
    
    Problem:
    Remove build result from git folder to build result folder
    
    Solution:
    add output folder from bb to Makefile,
    1.move aml_version.h to B folder
    2.if aml_version.h is suitable for the git commit ID, needn't update
    3.if never call version_config.sh, in configure.ac, it will create a
    default one from aml_version.h.in
    
    Verify:
    local
    
    Signed-off-by: guoping.li <guoping.li@amlogic.com>
    Change-Id: I27eca8a92689bd920d029014e6a169236a7bd4ee

--- version_config.sh
+++ version_config.sh
@@ -2,6 +2,12 @@
 
 #version rule:MAJORVERSION.MINORVERSION.COMMIT_COUNT-g(COMMIT_ID)
 
+OUT_DIR=
+if [ $# == 1 ];then
+    OUT_DIR=$1/
+    mkdir -p "${OUT_DIR}src"
+fi
+
 BASE=$(pwd)
 echo $BASE
 
@@ -25,8 +31,9 @@ echo commit count $COMMIT_COUNT
 COMMIT_ID=$(git rev-parse --short HEAD)
 echo commit id $COMMIT_ID
 
+cp "src/aml_version.h.in" "${OUT_DIR}src/aml_version.h.tmp"
 #find the module name line
-MODULE_NAME_LINE=`sed -n '/\"MM-module-name/=' src/aml_version.h`
+MODULE_NAME_LINE=`sed -n '/\"MM-module-name/=' ${OUT_DIR}src/aml_version.h.tmp`
 #echo $VERSION_LINE
 
 #version rule string
@@ -35,5 +42,22 @@ echo version: $VERSION_STRING
 
 #update the original version
 if [ ${MODULE_NAME_LINE} -gt 0 ]; then
-sed -i -e ${MODULE_NAME_LINE}s"/.*/\"${MODULE_NAME},version:${VERSION_STRING}\"\;/" src/aml_version.h
+sed -i -e ${MODULE_NAME_LINE}s"/.*/\"${MODULE_NAME},version:${VERSION_STRING}\"\;/" ${OUT_DIR}src/aml_version.h.tmp
+fi
+
+#if version.h already exist, compare the content, if it's the same, using the original file, avoid rebuild
+if [ -e "${OUT_DIR}src/aml_version.h" ]; then
+	file1_hash=$(md5sum ${OUT_DIR}src/aml_version.h | awk '{print $1}')
+	file2_hash=$(md5sum ${OUT_DIR}src/aml_version.h.tmp | awk '{print $1}')
+
+	if [ "$file1_hash" == "$file2_hash" ]; then
+		echo "version file content is the same"
+		rm ${OUT_DIR}src/aml_version.h.tmp
+	else
+		echo "version file content is not the same, use the tmp file"
+		mv ${OUT_DIR}src/aml_version.h.tmp ${OUT_DIR}src/aml_version.h
+	fi
+else
+	echo "no version file, use the tmp version file"
+	mv ${OUT_DIR}src/aml_version.h.tmp ${OUT_DIR}src/aml_version.h
 fi
